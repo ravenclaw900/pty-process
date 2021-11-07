a3b4ff4168b191cb89a207491908e59e029cbe4c
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-11-07 10:16:10 -0600

    Update std.rs

--- src/command/std.rs
+++ src/command/std.rs
@@ -31,6 +31,8 @@ impl super::CommandImpl for std::process::Command {
 
     fn spawn_impl(&mut self) -> ::std::io::Result<Self::Child> {
         println!("running spawn");
-        self.spawn()
+        let spawned = self.spawn();
+        println!("ran spawn");
+        spawned
     }
 }
